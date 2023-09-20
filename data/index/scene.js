da3a4952707e567793d58980bf648e5db378fe5d
ICG1001 <145522369+ICG1001@users.noreply.github.com>
2023-09-20 15:27:31 +1200

    Add files via upload

--- data/index/scene.js
+++ data/index/scene.js
@@ -14,7 +14,8 @@ app.loadJSONObject({
       "z": 0.0
     },
     "zScale": 1.0,
-    "light": "directional"
+    "light": "directional",
+    "proj": "+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
   },
   "layers": [
     {
